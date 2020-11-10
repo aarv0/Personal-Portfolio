3fc9981cf8a0e5d43e61f842cca1795420b5210e
Ali <calicorn00@gmail.com>
2020-11-10 11:29:59 -0700

    Update

--- starwars/js/main.js
+++ starwars/js/main.js
@@ -1,3 +1,116 @@
+import { people } from '../data/people.js'
+import { removeChildren, getLastNumber } from '../utils/index.js'
+
+const mainContent = document.querySelector('#main')
+
+populateDOM(people)
+
+const mainHeader = document.createElement('header')
+mainHeader.className = 'mainHeader'
+document.body.insertBefore(mainHeader, mainContent)
+
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+mainHeader.appendChild(maleButton)
+
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female Characters'
+mainHeader.appendChild(femaleButton)
+
+const otherButton = document.createElement('button')
+otherButton.textContent = 'Other Characters'
+mainHeader.appendChild(otherButton)
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+
+const femaleCharacters = people.filter(person => person.gender === 'female')
+
+const otherCharacters = people.filter(person => {
+    if (person.gender === 'n/a' ||
+        person.gender === 'none' ||
+        person.gender === 'hermaphrodite') {
+        return person
+    }
+})
+
+maleButton.addEventListener('click', (event) => {
+   populateDOM(maleCharacters)
+})
+
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
+
+otherButton.addEventListener('click', () => populateDOM(otherCharacters))
+
+function populateDOM(characters) {
+    removeChildren(mainContent)
+    characters.forEach(element => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        let charNum = getLastNumber(element.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        charImg.addEventListener('error', () => charImg.hidden = true) // genius level
+        const charCaption = document.createElement('figcaption')
+        charCaption.textContent = element.name
+    
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
+    
+        mainContent.appendChild(charFigure)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import {films}     from "../data/films.js";
 import {people}    from "../data/people.js";
 import {planets}   from "../data/planets.js";
@@ -175,3 +288,4 @@ starships_button.addEventListener("click", function () {
 vehicles_button.addEventListener("click", function () {
     changeWiki(vehicles_button, wiki_vehicles);
 });
+*/
