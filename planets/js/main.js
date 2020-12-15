98beb78803d7e1dae450476cefd00068b7a2844f
Ali <calicorn00@gmail.com>
2020-12-14 21:52:56 -0700

    Got planets to import

--- planets/js/main.js
+++ planets/js/main.js
@@ -1,28 +1,79 @@
 import { planets } from '../data/planets.js'
-  
+ 
+
+
 let mainHeader = document.querySelector('header')
 let mainArea = document.querySelector('main')
 
 
-planets.forEach((planet)=> {
 
-    let planetDiv = document.createElement ('div')
-    let name = document.createElement ('h1')
+planets.forEach((planet) => {
+
+    let planetDiv = document.createElement('div')
     let pic = document.createElement('img')
-    
-
+    let name = document.createElement('h1')
+    pic.setAttribute('class', 'planetPic')
     name.textContent = planet.name
-    pic.src = `https://swapi.dev/api/planets/${planNum}.jpg`
-    /*ic.src = `https://starwars-visualguide.com/assets/img/planets/${planetNum}.jpg`*/
-
-    pic.setAttribute('class', 'card')
-
+    let planetNum = getPlanetNum(planet.url)
+    mainArea.appendChild(planetDiv)
     planetDiv.appendChild(name)
     planetDiv.appendChild(pic)
+    
+    pic.src = `https://starwars-visualguide.com/assets/img/planets/${planetNum}.jpg`
+     
+    
 
-    mainArea.appendChild(planetDiv)
+    planetDiv.setAttribute('class', 'planetDiv')
+    name.setAttribute('class', 'planetName')
+    function getPlanetNum(planetURL) {
+        console.log(planetURL)
+        let end = planetURL.lastIndexOf('/')
+        let planetID = (planetURL.substring(end - 2, end))
+        if (planetID.indexOf('/') !== -1) {
+            return (planetID.slice(1, 2))
+        } else {
+            return planetID
+        }
+    
+    }
 
+    
 })
+
+
+
+
+
+
+
+
+/*
+planets.forEach(function(planet) {
+
+        let planetDiv = document.createElement('div')
+        let planetName = document.createElement('h1')
+        let planetPic = document.createElement('img')
+
+        
+        planetDiv.appendChild(name)
+        planetDiv.appendChild(pic)
+
+
+        let planetNum = getPlanetNumber(planet.url)
+
+        planetName.textContent = planet.name
+        /*planetPic.src = `https://swapi.dev/api/planets/${planetNum}.jpg`*/
+
+        /*pic.src = `https://starwars-visualguide.com/assets/img/planets/${planetNum}.jpg`*/
+       
+       
+        /*pic.setAttribute('class', 'card')
+
+       
+
+        mainArea.appendChild(planetDiv)
+
+    })
 
 
 
@@ -35,20 +86,22 @@ planetButton.textContent = 'Planets'
 planetButton.addEventListener('click', () => {
     planets.forEach(planet => {
         let matchedDiv = allDivs.find(oneDiv => {
-          return oneDiv.firstChild.textContent === planet.name;
+          return oneDiv.firstChild.textContent === planet.name
         })
-        matchedDiv.classList.add("animated", "fadeOut")
+        matchedDiv[0].setAttribute("style", "display: none;")
+        /*matchedDiv.classList.add("animated", "fadeOut")
         })
     }) 
 
 mainHeader.appendChild(planetButton)
 
 
-
-
  
 
-/*
+
+
+
+
 
 planets.forEach((planet)=> {
 
@@ -96,44 +149,8 @@ planetButton.addEventListener('click', () => {
 */
 
 
+
 /*
-planets.forEach((planet) => {
-
-    let planetDiv = document.createElement('div')
-    let pic = document.createElement('img')
-    let name = document.createElement('h1')
-    pic.setAttribute('class', 'planetPic')
-    name.textContent = planet.name
-    let planetNum = getPlanetNum(planet.url)
-    mainArea.appendChild(planetDiv)
-    planetDiv.appendChild(name)
-    planetDiv.appendChild(pic)
-    
-    pic.src = `https://starwars-visualguide.com/assets/img/planets/${planetNum}.jpg`
-    
-    
-
-    planetDiv.setAttribute('class', 'planetDiv')
-    name.setAttribute('class', 'planetName')
-    function getPlanetNum(planetURL) {
-        console.log(planetURL)
-        let end = planetURL.lastIndexOf('/')
-        let planetID = (planetURL.substring(end - 2, end))
-        if (planetID.indexOf('/') !== -1) {
-            return (planetID.slice(1, 2))
-        } else {
-            return planetID
-        }
-    
-    }
-    
-    pic.addEventListener('error', (event) => {
-        let badImage = event.target
-        badImage.src = '../images/Star_Wars_logo-1.png'
-      })
-})
-
-
 
 const mainHeader = document.querySelector('header')
 const allDivs = Array.from(document.querySelectorAll('div'));
@@ -150,33 +167,7 @@ planetsButton.addEventListener('click', () => {
         })
         matchedDiv.setAttribute("style", "display: block;")
     })
-    maleChar.forEach(character => {
-        let matchedDiv = allDivs.find((oneDiv) => {
-            return oneDiv.firstChild.textContent === character.name
-        })
-        matchedDiv.setAttribute('style', 'display: none')
-        matchedDiv.classList.add('animated', 'display:none')
-    })
 
-    femaleChar.forEach(character => {
-        let matchedDiv = allDivs.find((oneDiv) => {
-            return oneDiv.firstChild.textContent === character.name
-        })
-        matchedDiv.setAttribute('style', 'display:none')
-        otherChar.forEach(character => {
-            let matchedDiv = allDivs.find((oneDiv) => {
-                return oneDiv.firstChild.textContent === character.name
-            })
-            matchedDiv.setAttribute('style', 'display:none')
-        })
-
-    })
-    
-    })
-  
-  
-  
-  
   
   
   
